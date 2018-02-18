cb5fcafd4e5b000d1d9f1154cd880dfee1d22a26
RIscRIpt <richard@riscript.com>
2018-02-18 19:06:44 +0200

    move template specialization of connection shift to cpp file

--- connection.cpp
+++ connection.cpp
@@ -7,3 +7,21 @@ Connection::Connection(int sockfd)
 {}
 
 Connection::~Connection() {}
+
+template<>
+Connection& Connection::operator<<(std::string const &value) {
+    uint64_t size = value.size();
+    send(reinterpret_cast<std::byte const*>(&size), sizeof(size));
+    send(reinterpret_cast<std::byte const*>(value.data()), size);
+    return *this;
+}
+
+template<>
+Connection& Connection::operator>>(std::string &value) {
+    uint64_t size = 0;
+    recv(reinterpret_cast<std::byte*>(&size), sizeof(size));
+    value.resize(size);
+    recv(reinterpret_cast<std::byte*>(value.data()), size);
+    return *this;
+}
+
--- connection.h
+++ connection.h
@@ -27,26 +27,9 @@ namespace rrl {
             return *this;
         }
 
-        template<>
-        Connection& operator<<(std::string const &value) {
-            uint64_t size = value.size();
-            send(reinterpret_cast<std::byte const*>(&size), sizeof(size));
-            send(reinterpret_cast<std::byte const*>(value.data()), size);
-            return *this;
-        }
-
         template<typename T>
         Connection& operator>>(T &value) {
             recv(reinterpret_cast<std::byte*>(&value), sizeof(T));
-            return *this;
-        }
-
-        template<>
-        Connection& operator>>(std::string &value) {
-            uint64_t size = 0;
-            recv(reinterpret_cast<std::byte*>(&size), sizeof(size));
-            value.resize(size);
-            recv(reinterpret_cast<std::byte*>(value.data()), size);
             return *this;
         }
 
@@ -55,3 +38,4 @@ namespace rrl {
     };
 
 }
+
